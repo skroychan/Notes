b2fda95ab71adeb5bcd92234c09ea1d136752e59
skroychan <skroychan@gmail.com>
2024-02-17 02:40:25 +0400

    messagebox unhandled exceptions

--- NotesDesktop/App.xaml.cs
+++ NotesDesktop/App.xaml.cs
@@ -1,7 +1,14 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace skroy.NotesDesktop;
 
 public partial class App : Application
 {
+	protected override void OnStartup(StartupEventArgs e)
+	{
+		base.OnStartup(e);
+
+		AppDomain.CurrentDomain.UnhandledException += (_, e) => MessageBox.Show(((Exception)e.ExceptionObject).Message);
+	}
 }
